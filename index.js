b87bf6f614cc32fc9f6f8b83909ac58bc0c6c795
batuta <saelbatita@gmail.com>
2020-10-06 09:14:14 +0200

    add moule database

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const 
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const database = require('./database/config');
+const port = 3000;
+
+const app = express();
+
+//connexion in database
+database.sync().then((resul)=>{
+    console.log("connexion");
+}).catch((erro)=>{
+    console.error("ça marche");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
